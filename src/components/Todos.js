d110ed43b2954528d72020ce64907d1ed082f659
Collin Smith <cmsmith.tx@gmail.com>
2019-07-31 13:09:14 -0500

    initial commit

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render(){
+        return (
+            <div>
+                <h1>To-Dos</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
