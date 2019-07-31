96d22a412a25b0d0ebf240e7fe37b6c2102f6cc8
Collin Smith <cmsmith.tx@gmail.com>
2019-07-31 16:03:56 -0500

    Added About page and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About'
 import uuid from 'uuid';
 import './App.css';
 
@@ -61,13 +63,20 @@ addTodo = (title) => {
   render(){
 
   return (
+    <Router>
     <div className="App">
       <div className="container">
       <Header />
-      <AddTodo addTodo={this.addTodo} />
-      <Todos todos={this.state.todos} markComplete={ this.markComplete } delTodo={this.delTodo} />
+      <Route exact path="/" render={props => (
+        <React.Fragment>
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} markComplete={ this.markComplete } delTodo={this.delTodo} />
+        </React.Fragment>
+      )} />
+      <Route path="/about" component={About} />
     </div>
     </div>
+    </Router>
   );
 }
 }
